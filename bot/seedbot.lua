86de6751c4e82e9df9f4635c0db310617151c8c8
hacker1383 <mhadisalari1383@gmail.com>
2016-07-14 12:19:50 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -227,7 +227,8 @@ function create_config( )
     "admin",
     "Voice",
     "Photo",
-    "Sticker
+    "Sticker",
+    "Linkpv"
     },
     sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
@@ -435,6 +436,11 @@ permutational photo to sticker
 !photo 
 permutational sticker to photo
 (By reply)
+
+!linkpv
+Get link in your pv!
+
+
 
 
 
