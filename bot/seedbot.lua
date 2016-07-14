0a31c9a7d2fdbc049bac7260eb0efd100dc1a135
hacker1383 <mhadisalari1383@gmail.com>
2016-07-14 12:58:02 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -228,7 +228,8 @@ function create_config( )
     "Voice",
     "Photo",
     "Sticker",
-    "Linkpv"
+    "Linkpv",
+    "Weather"
     },
     sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
